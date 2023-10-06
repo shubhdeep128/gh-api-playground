f3a6ae9c959ef3fb18a9cd3c82d49bcd45721c1e
Shubh Deep <shubh.deep@razorpay.com>
2023-10-06 20:40:34 +0530

    Debug

--- .github/scripts/access_sheet.py
+++ .github/scripts/access_sheet.py
@@ -4,26 +4,29 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 def check_blocked_status(github_id):
-    creds_json = json.loads(os.environ['GOOGLE_SHEETS_CREDS'])
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_json, ["https://spreadsheets.google.com/feeds"])
-    
-    client = gspread.authorize(creds)
+    creds_base64 = os.environ['GOOGLE_SHEETS_CREDS']
+    creds_json_str = base64.b64decode(creds_base64).decode('utf-8')
+    creds_json = json.loads(creds_json_str)
+    print(creds_json)
+    client = gspread.authorize(creds_json)
     
     # Use the spreadsheet ID to open the spreadsheet
     spreadsheet_id = os.environ['SPREADSHEET_ID']
+    print(spreadsheet_id)
     spreadsheet = client.open_by_key(spreadsheet_id)
     
     # Use the sheet ID (gid) to select the specific sheet
     sheet_id = os.environ['SHEET_ID']
+    print(sheet_id)
     sheet = [worksheet for worksheet in spreadsheet.worksheets() if worksheet.id == sheet_id][0]
     
     # Search for the GitHub ID in the sheet
     cell = sheet.find(github_id)
-    
+    print(cell.row)
     # Check the status in the corresponding row (assuming status is in Column C)
-    status = sheet.cell(cell.row, 3).value
+    isBlocked = sheet.cell(cell.row, 3).value
     
-    if status == "Blocked":
+    if isBlocked == "TRUE":
         print(f"::error::User {github_id} is blocked from merging!")
         exit(1)
 
